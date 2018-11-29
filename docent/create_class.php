0bb3cb9cda61184622ce58f3a9840121bbf5fa88
Diderickm <Diderick@live.nl>
2018-11-29 21:55:16 +0100

    Add HotJar

--- docent/create_class.php
+++ docent/create_class.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+
+<!-- Hotjar Tracking Code for https://skiffle.nl -->
+    <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1109995,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Tickets</title>
