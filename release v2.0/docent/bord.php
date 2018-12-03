5eb257a608a4f64e8f11d04d28f923254439a2bb
Diderickm <Diderick@live.nl>
2018-12-03 21:12:14 +0100

    sql

--- release v2.0/docent/bord.php
+++ release v2.0/docent/bord.php
@@ -37,7 +37,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once('../conn.php');
+include('../conn.php');
 
 function generateRandomString($length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
