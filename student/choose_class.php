bc43d62227b891865a617d274cc9e93a820b52ef
DiderickM <Diderick@live.nl>
2019-01-27 23:19:50 +0100

    ADDD EMOJI

--- student/choose_class.php
+++ student/choose_class.php
@@ -20,6 +20,10 @@
     <link type="text/css" rel="stylesheet" href="../css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <!-- emoji -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/css/emoji.css" rel="stylesheet">
+    <!-- end emoji -->
 </head>
 <style>
     
@@ -35,19 +39,39 @@
                 <span class="focus-border"></span>
             </div>
 
-            <!--<input type="text" name="naam" placeholder="Johan"><br>-->
-
             <div class="pos input-effect">
-                <input id="input2" name="naam" class="input effect" type="text" placeholder="">
+                <input data-emojiable="true" data-emoji-input="unicode" id="input2" name="naam" class="input effect" type="text" placeholder="" maxlength="15">
                 <label>Naam</label>
                 <span class="focus-border"></span>
             </div>
+
             <div class="btncenter">
                 <input style="width: 100%" class="btn blue" type="submit" value="Join!"><br>
                 <a href="../" class="btn red">Terug</a>
             </div>
         </form>
     </div>
+      <!-- ** Don't forget to Add jQuery here ** -->
+  <script src="lib/js/config.js"></script>
+  <script src="lib/js/util.js"></script>
+  <script src="lib/js/jquery.emojiarea.js"></script>
+  <script src="lib/js/emoji-picker.js"></script>
+
+  
+  <script>
+      $(function() {
+        // Initializes and creates emoji set from sprite sheet
+        window.emojiPicker = new EmojiPicker({
+          emojiable_selector: '[data-emojiable=true]',
+          assetsPath: 'lib/img/',
+          popupButtonClasses: 'fa fa-smile-o'
+        });
+        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+        // It can be called as many times as necessary; previously converted input fields will not be converted again
+        window.emojiPicker.discover();
+      });
+    </script>
 </body>
 </html>
 <script>
