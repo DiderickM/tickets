0dae5eff87894fb87010dbe709b567b32986f304
DiderickM <Diderick@live.nl>
2019-01-28 00:04:03 +0100

    Fixed issue input

--- student/choose_class.php
+++ student/choose_class.php
@@ -56,22 +56,6 @@
   <script src="lib/js/util.js"></script>
   <script src="lib/js/jquery.emojiarea.js"></script>
   <script src="lib/js/emoji-picker.js"></script>
-
-  
-  <script>
-      $(function() {
-        // Initializes and creates emoji set from sprite sheet
-        window.emojiPicker = new EmojiPicker({
-          emojiable_selector: '[data-emojiable=true]',
-          assetsPath: 'lib/img/',
-          popupButtonClasses: 'fa fa-smile-o'
-        });
-        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
-        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
-        // It can be called as many times as necessary; previously converted input fields will not be converted again
-        window.emojiPicker.discover();
-      });
-    </script>
 </body>
 </html>
 <script>
@@ -82,7 +66,7 @@
 		
 		$(".input-effect input").focusout(function(){
 			if($(this).val() != ""){
-				$(this).addClass("has-content");
+                $(this).addClass("has-content");
 			}else{
 				$(this).removeClass("has-content");
 			}
@@ -90,15 +74,32 @@
 
         var myInput1 = document.getElementById("input");
         if (myInput1 && myInput1.value) {
-            
             $('#input').addClass("has-content");
-            
         }
 
-        var myInput2 = document.getElementById("input2");
-        if (myInput2 && myInput2.value) {
+        window.setInterval(function(){
+        /// call your function here
+        var e = document.getElementById('input2');
+        
+        if(e.value != ""){
+            console.log(e.value);
             $('#input2').addClass("has-content");
+        }else{
+            $('#input2').removeClass("has-content");
         }
+        }, 100);
+
+         // Initializes and creates emoji set from sprite sheet
+        window.emojiPicker = new EmojiPicker({
+        emojiable_selector: '[data-emojiable=true]',
+        assetsPath: 'lib/img/',
+        popupButtonClasses: 'fa fa-smile-o'
+        });
+        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+        // It can be called as many times as necessary; previously converted input fields will not be converted again
+        window.emojiPicker.discover();
 
 	});
 </script>
+
